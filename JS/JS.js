f4c57fd5cb33918969b2efe2a8badc80b802b241
Greeny867 <46565788+Greeny867@users.noreply.github.com>
2019-01-10 13:40:46 +0000

    Add files via upload

---
+++ JS/JS.js
@@ -0,0 +1,22 @@
+localStorage.getItem('background_color');
+// Store
+localStorage.setItem("color", "#");
+// Retrieve
+document.getElementById("result").innerHTML = localStorage.getItem("background"); 
+
+if(localStorage && localStorage.getItem('background_color')){ var colour = localStorage.getItem('background_color'); 
+alert('colour should be '+color); 
+//change css background color 
+
+<script>
+document.getElementById("myBtn").addEventListener("click", displayColor);
+
+function displayColor() {
+    document.getElementById("demo").innerHTML = Date();
+}
+</script>
+$('#selColor').change(function(){
+
+console.log($('#selColor').val(displayColor)); 
+//change the body css backround  
+//set the local storage  localStorage.setItem(name, value); }); 
